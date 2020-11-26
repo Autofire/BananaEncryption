e6fc4d3b3c5b9b0c057f8afa8d57fe824324ed49
Autofire <Autofire@users.noreply.github.com>
2020-11-25 21:32:04 -0800

    Did some WIP message passing

--- src/main_state.rs
+++ src/main_state.rs
@@ -1,10 +1,43 @@
 use orbtk::prelude::*;
 
+use crate::MainView;
+
+pub enum Message {
+	Encrypt(String),
+	Decrypt(String),
+	Clear,
+}
+
 #[derive(Default, AsAny)]
 pub struct MainState {
+	path: String
 }
 
 impl State for MainState {
-    fn update(&mut self, _: &mut Registry, _: &mut Context) {
-    }
+	//fn update(&mut self, _: &mut Registry, _: &mut Context) {
+	//}
+
+	fn messages(
+			&mut self,
+			mut messages: MessageReader,
+			_registry: &mut Registry,
+			ctx: &mut Context,
+	) {
+
+		for message in messages.read::<Message>() {
+			match message {
+				Message::Encrypt(p) => {
+					println!("Encrypt: {}", p);
+					self.path = p.clone();
+					MainView::target_file_set(&mut ctx.widget(), p);
+				},
+				Message::Decrypt(p) => {
+					println!("Decrypt: {}", p);
+				},
+				Message::Clear => {
+					println!("Clear");
+				}
+			}
+		}
+	}
 }
--- src/main_view.rs
+++ src/main_view.rs
@@ -1,17 +1,33 @@
 use orbtk::prelude::*;
 
-use crate::MainState;
+use crate::{MainState, Message};
 
 widget!(
 	DropArea: DropHandler {
+		text: String
 	}
 );
 
 impl Template for DropArea {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+
+		let test = Message::Encrypt(String::from("Hi"));
+
 		self.name("Drop Area")
-			.child(ImageWidget::new()
-				.image("assets/orbtk_logo.png")
+			.text("Hello")
+			.child(Stack::new()
+				.margin(10)
+				.spacing(10)
+				.child(ImageWidget::new()
+					.image("assets/dnd.png")
+					.h_align("center")
+					.build(ctx)
+				)
+				.child(TextBlock::new()
+					.text(id)
+					.h_align("center")
+					.build(ctx)
+				)
 				.build(ctx)
 			)
 
@@ -19,88 +35,36 @@ impl Template for DropArea {
 
 }
 
-widget!(
-    MyWidget: DropHandler {
-      background: Brush,
-      count: u32,
-      text: String
-    }
-);
-
-impl Template for MyWidget {
-    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-		let pager = Pager::new()
-			.child(TextBlock::new().text("Page 1").build(ctx))
-			.child(TextBlock::new().text("Page 2").build(ctx))
-			.build(ctx);
-
-		let next_button = Button::new()
-			.enabled(("next_enabled", pager))
-			.text("next")
-			.on_click(move |states, _| {
-					//states.get_mut::<PagerState>(pager).next();
-					states.send_message(PagerAction::Next, pager);
-					//orbtk::widgets::Pager::next(ctx, pager);
-					true
-					})
-			.build(ctx);
-
-		let previous_button = Button::new()
-			.enabled(("previous_enabled", pager))
-			.text("previous")
-			.on_click(move |states, _| {
-					//states.get_mut::<PagerState>(pager).previous();
-					states.send_message(PagerAction::Previous, pager);
-					true
-			})
-			.build(ctx);
-
-		self.name("MyWidget")
-			.style("my_widget_style")
-			.background("#000000")
-			.count(0)
-			.text("Initial text HIII")
-			.on_drop_file(|_,path,_| {
-					println!("event triggered");
-					println!("{}", path);
-					true
-					})
-					/*
-			.child(Container::new()
-				// Container references the same background as MyWidget
-				.background(id)
-				.child(TextBlock::new()
-					// TextBlock references the same text as MyWidget
-					.text(id)
-					.build(ctx)
-				)
-				.build(ctx)
-			  )
-			  */
-			.child(Stack::new()
-				.width(120)
-				.margin(10)
-				.spacing(10)
-				.child(pager)
-				.child(next_button)
-				.child(previous_button)
-				.build(ctx)
-			)
-    }
-}
-
 
 widget!(
     MainView<MainState> {
-        title: String
+        title: String,
+		target_file: String
     }
 );
 
 impl Template for MainView {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+
+		let drop_page = DropArea::new()
+			.on_drop_file(move |states,path,_| {
+				println!("event triggered");
+				println!("{}", path);
+				//DropArea::text_set(&mut sctx.get(), path);
+				states.send_message(Message::Encrypt(path), id);
+				true
+			})
+			.text(("target_file", id))
+			.v_align("center")
+			.h_align("center")
+			.build(ctx);
+
+
         self.name("MainView")
+			.target_file("None")
 			//.child(TextBlock::new().text(("title", id)).build(ctx))
 			//.child(MyWidget::new().build(ctx))
+			/*
 			.child(DropArea::new()
 				.on_drop_file(|_,path,_| {
 					println!("event triggered");
@@ -111,5 +75,24 @@ impl Template for MainView {
 				.h_align("center")
 				.build(ctx)
 			)
+			*/
+			.child(drop_page)
     }
 }
+
+	/*
+		let pager = Pager::new()
+			.child(TextBlock::new().text("Page 1").build(ctx))
+			.child(TextBlock::new().text("Page 2").build(ctx))
+			.build(ctx);
+
+		let next_button = Button::new()
+			.enabled(("next_enabled", pager))
+			.text("next")
+			.on_click(move |states, _| {
+					states.send_message(PagerAction::Next, pager);
+					true
+					})
+			.build(ctx);
+	*/
+
