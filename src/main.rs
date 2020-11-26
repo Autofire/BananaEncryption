a41c8ea3a24fb94c80e223d3596b6ac0a4719ade
Autofire <Autofire@users.noreply.github.com>
2020-11-25 17:41:14 -0800

    Made simple  drop area

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
             Window::new()
                 .title("orbtk-sandbox")
                 .position((100.0, 100.0))
-                .size(372.0, 768.0)
+                .size(300.0, 300.0)
                 .resizeable(true)
                 .child(MainView::new().title("Hello OrbTk").build(ctx))
                 .build(ctx)
--- src/main_view.rs
+++ src/main_view.rs
@@ -3,10 +3,21 @@ use orbtk::prelude::*;
 use crate::MainState;
 
 widget!(
-    MainView<MainState> {
-        title: String
-    }
+	DropArea: DropHandler {
+	}
 );
+
+impl Template for DropArea {
+    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+		self.name("Drop Area")
+			.child(ImageWidget::new()
+				.image("assets/orbtk_logo.png")
+				.build(ctx)
+			)
+
+	}
+
+}
 
 widget!(
     MyWidget: DropHandler {
@@ -18,32 +29,87 @@ widget!(
 
 impl Template for MyWidget {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-         self.name("MyWidget")
-            .style("my_widget_style")
-            .background("#000000")
-            .count(0)
-            .text("Initial text HIII")
-			.on_drop_file(|_,_,_| {
-				println!("event triggered");
-				true
+		let pager = Pager::new()
+			.child(TextBlock::new().text("Page 1").build(ctx))
+			.child(TextBlock::new().text("Page 2").build(ctx))
+			.build(ctx);
+
+		let next_button = Button::new()
+			.enabled(("next_enabled", pager))
+			.text("next")
+			.on_click(move |states, _| {
+					//states.get_mut::<PagerState>(pager).next();
+					states.send_message(PagerAction::Next, pager);
+					//orbtk::widgets::Pager::next(ctx, pager);
+					true
+					})
+			.build(ctx);
+
+		let previous_button = Button::new()
+			.enabled(("previous_enabled", pager))
+			.text("previous")
+			.on_click(move |states, _| {
+					//states.get_mut::<PagerState>(pager).previous();
+					states.send_message(PagerAction::Previous, pager);
+					true
 			})
-            .child(Container::new()
-                    // Container references the same background as MyWidget
-                    .background(id)
-                    .child(TextBlock::new()
-                            // TextBlock references the same text as MyWidget
-                            .text(id)
-                            .build(ctx)
-                    )
-                    .build(ctx)
-            )
+			.build(ctx);
+
+		self.name("MyWidget")
+			.style("my_widget_style")
+			.background("#000000")
+			.count(0)
+			.text("Initial text HIII")
+			.on_drop_file(|_,path,_| {
+					println!("event triggered");
+					println!("{}", path);
+					true
+					})
+					/*
+			.child(Container::new()
+				// Container references the same background as MyWidget
+				.background(id)
+				.child(TextBlock::new()
+					// TextBlock references the same text as MyWidget
+					.text(id)
+					.build(ctx)
+				)
+				.build(ctx)
+			  )
+			  */
+			.child(Stack::new()
+				.width(120)
+				.margin(10)
+				.spacing(10)
+				.child(pager)
+				.child(next_button)
+				.child(previous_button)
+				.build(ctx)
+			)
     }
 }
+
+
+widget!(
+    MainView<MainState> {
+        title: String
+    }
+);
 
 impl Template for MainView {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
         self.name("MainView")
-			.child(TextBlock::new().text(("title", id)).build(ctx))
-			.child(MyWidget::new().build(ctx))
+			//.child(TextBlock::new().text(("title", id)).build(ctx))
+			//.child(MyWidget::new().build(ctx))
+			.child(DropArea::new()
+				.on_drop_file(|_,path,_| {
+					println!("event triggered");
+					println!("{}", path);
+					true
+				})
+				.v_align("center")
+				.h_align("center")
+				.build(ctx)
+			)
     }
 }
